11a892ab3440a4e29f40da9a1c8fb605a81bfef9
choteit <58633741+choteit@users.noreply.github.com>
2025-07-24 10:23:36 +0000

    Added auth persistance for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'users/services/auth_gate.dart';
 import 'utils/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Enable Firebase Auth persistence for web
+  await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+
   runApp(const MyApp());
 }
 
